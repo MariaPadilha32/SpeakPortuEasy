758b60836bbbfe3f27f2b9b2ec9895f71bf62ae4
MARIA FERNANDA DIAS PADILHA <137770409+MariaPadilha32@users.noreply.github.com>
2024-02-18 02:46:49 +0000

    To configurate views.py to direct to index.html.

--- main/views.py
+++ main/views.py
@@ -5,30 +5,3 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'base.html')
     #return HttpResponse('Hello World - Welcome to Django Framework')
-
-def register_student(request):
-    return render(request, 'register-student.html')
-
-def register_class(request):
-    return render(request, 'register-class.html')
-
-def register_schedule(request):
-    return render(request, 'register-schedule.html')
-
-def register_classroom(request):
-    return render(request, 'register-classroom.html')
-
-def register_enrollments(request):
-    return render(request, 'register-classroom.html')
-
-def register_users(request):
-    return render(request, 'register-users.html')
-
-def register_profiles(request):
-    return render(request, 'register-profiles.html')
-
-def register_parents(request):
-    return render(request, 'register-parents.html')
-
-def register_zipcode(request):
-    return render(request, 'register-zipcode.html')
