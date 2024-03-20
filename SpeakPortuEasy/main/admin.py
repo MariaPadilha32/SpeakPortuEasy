042ccffbe195266b590ef8fddc28b5399b952e13
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-03-20 01:20:30 +0000

    update admin.py

--- SpeakPortuEasy/main/admin.py
+++ SpeakPortuEasy/main/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Classroom, Classes
+from .models import Classroom, Classes, ZipCode, Enrollments, Parents, Student, Profiles, Schedule
 
 # Register your models here.
 admin.site.register(Classroom)
 admin.site.register(Classes)
+admin.site.register(ZipCode)
+admin.site.register(Enrollments)
+admin.site.register(Parents)
+admin.site.register(Student)
+admin.site.register(Profiles)
+admin.site.register(Schedule)
