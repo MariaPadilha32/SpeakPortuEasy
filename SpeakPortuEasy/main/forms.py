7e39fe69f3fab89465e0e149cf40935b0e535755
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-03-20 02:08:41 +0000

    Add information on forms.py

--- SpeakPortuEasy/main/forms.py
+++ SpeakPortuEasy/main/forms.py
@@ -1,7 +1,42 @@
 from django.forms import ModelForm
-from .models import Classroom
+from .models import Classroom, Classes, ZipCode, Enrollments, Parents, Student, Profiles, Schedule
 
 class ClassroomForm(ModelForm):
     class Meta: # using Meta database
         model = Classroom  
+        fields = '__all__'
+
+class ClassesForm(ModelForm):
+    class Meta:
+        model = Classes
+        fields = '__all__'
+
+class ZipCodeForm(ModelForm):
+    class Meta:
+        model = ZipCode
+        fields = '__all__'
+
+class EnrollmentsForm(ModelForm):
+    class Meta:
+        model = Enrollments
+        fields = '__all__'
+
+class ParentsForm(ModelForm):
+    class Meta:
+        model = Parents
+        fields = '__all__'
+
+class StudentForm(ModelForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+class ProfilesForm(ModelForm):
+    class Meta:
+        model = Profiles
+        fields = '__all__'
+
+class ScheduleForm(ModelForm):
+    class Meta:
+        model = Schedule
         fields = '__all__'
