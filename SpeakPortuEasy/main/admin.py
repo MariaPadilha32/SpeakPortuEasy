a1687a7e7cdf04d76e562985924a9bbbd7c3418b
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-03-20 00:45:07 +0000

    Create model for Class

--- SpeakPortuEasy/main/admin.py
+++ SpeakPortuEasy/main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Classroom
+from .models import Classroom, Classes
 
 # Register your models here.
 admin.site.register(Classroom)
+admin.site.register(Classes)
