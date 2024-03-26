396041fddc9ce2fdde54dcf11eff318baf360ce4
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-03-26 22:36:51 +0000

    migrate deletition on zipcode

--- SpeakPortuEasy/main/admin.py
+++ SpeakPortuEasy/main/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import Classroom, Classes, ZipCode, Enrollments, Parents, Student, Profiles, Schedule, Teacher
+from .models import Classroom, Classes, Enrollments, Parents, Student, Profiles, Schedule, Teacher
 
 # Register your models here.
 admin.site.register(Classroom)
 admin.site.register(Classes)
-admin.site.register(ZipCode)
 admin.site.register(Enrollments)
 admin.site.register(Parents)
 admin.site.register(Student)
