fe13ab08d92782dc6f3189378740029fc65bab5f
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-10-29 21:52:17 +0000

    removing secret Key and add env.py to gitignore

--- SpeakPortuEasy/settings.py
+++ SpeakPortuEasy/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_cdca#n@1(p1t*f9+yd&7@)(6%lz&u^!jb(!3!)4%rj5u93@q%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- env.py
+++ env.py
@@ -1,1 +1,3 @@
-SECRET_KEY=')^4n33=m8-2i5lp2%%rn7*y!-^70q8w_th469-cibvb0r(wgf#'
+import os
+
+os.environ['SECRET_KEY'] = ')^4n33=m8-2i5lp2%%rn7*y!-^70q8w_th469-cibvb0r(wgf#'
