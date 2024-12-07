d8cc193db36b50ea74a913cfb0146c2750b338ec
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-12-07 21:32:24 +0000

    set debug to false for production environment

--- SpeakPortuEasy/settings.py
+++ SpeakPortuEasy/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'speakportueasypp4-342d78e3516e.herokuapp.com', 
