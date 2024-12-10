57c47fd8740adcd6a459b7eab6dc8584d438e004
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-12-10 21:57:09 +0000

    Refactored JavaScript for code consistency and readability

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,17 @@
 console.log('teste de script');
-function control_under_age(){
-    document.querySelector('#txtParents').disabled = !(document.querySelector('#txtUnderAge').checked)
-};
 
+function control_under_age() {
+    document.querySelector('#txtParents').disabled = !document.querySelector('#txtUnderAge').checked;
+}
 
-function numbers_only(pElemento)
-{
-    pElemento.addEventListener('keydown',function(e){
-        if (!/[0-9]/.test(e.key) && e.key !== 'Backspace' && e.key !== 'Tab')
-            e.preventDefault()
-    })
+function numbers_only(pElemento) {
+    pElemento.addEventListener('keydown', function (e) {
+        if (!/[0-9]/.test(e.key) && e.key !== 'Backspace' && e.key !== 'Tab') {
+            e.preventDefault();
+        }
+    });
 
-    pElemento.addEventListener('input', function(){
-        this.value = this.value.replace(/\D/g,'')
-    })
+    pElemento.addEventListener('input', function () {
+        this.value = this.value.replace(/\D/g, '');
+    });
 }
